d6d2dd664f734041cf9890856c8560811d01c009
Grijalva <grijalva19@gmail.com>
2020-01-24 15:15:13 -0500

    Added basic structure to the project according to step 1 "Project Structure"

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//IIFE - Immediately Invoked Function Expression
+// mean? -> anonymous self-executing function
+(function(){
+
+    function Start() {
+        
+    }
+    window.addEventListener("load", Start);
+})();
