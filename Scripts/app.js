3679b878036874d0a5c29321f643861fdcf3f16d
Grijalva <grijalva19@gmail.com>
2020-01-24 15:19:58 -0500

    Changed "Products" to "Projects link" (Step 2.1)

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,7 +4,10 @@
 (function(){
 
     function Start() {
-        
+        //Get element
+        let products = document.getElementById("products");
+        //Get the "a" tag as a child from products and change its text
+        products.lastChild.lastChild.textContent = " Projects";
     }
     window.addEventListener("load", Start);
 })();
